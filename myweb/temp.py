d242bcbf5285bfad2c90d4e97791ce11848dab9d
yongdukkim <ydkim@logishub.net>
2018-01-13 16:00:16 -0500

    ^^

---
+++ myweb/temp.py
@@ -0,0 +1,15 @@
+a = 2
+
+if a != 3:
+    print("a != 3")
+else:
+    print("a == 3")
+
+a = [1, 2, 3, 4]
+b = 5 in a    # True or False
+print(b)
+
+s = "ABC"
+print(type(s))      # class 'str'
+v = s[1]     # B
+print(type(s[1]))    # class 'str'
